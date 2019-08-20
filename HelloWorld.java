1a625172729d3852ee2779c01d81bfee576bb544
longlong <1149208788@qq.com>
2019-08-20 12:35:43 +0800

    hellogit

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld{
+	
+	public static void main(String []arg){
+		System.out.println("Hello Git");
+		System.out.println("1");
+		System.out.println("Hello Git");
+		System.out.println("2");
+		
+	}
+}
